9563b680d34255ca6ccc707ef038e59cfccfb1e7
Gonçalo Ferreira <goncalo.ferreira@datathings.com>
2024-07-17 21:30:19 +0200

    Changed cap price to 2000 for Vesteda

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { Agency, type AgencyUrlMap } from "./types";
 
 export const VESTEDA_SEARCH_URL =
-  "https://www.vesteda.com/en/unit-search?placeType=1&sortType=1&radius=20&s=Amsterdam&sc=woning&latitude=52.36757278442383&longitude=4.904139041900635&filters=&priceFrom=500&priceTo=1500";
+  "https://www.vesteda.com/en/unit-search?placeType=1&sortType=1&radius=20&s=Amsterdam&sc=woning&latitude=52.36757278442383&longitude=4.904139041900635&filters=&priceFrom=500&priceTo=2000";
 
 export const FUNDA_SEARCH_URL = 
   `https://www.funda.nl/zoeken/huur?selected_area=["amsterdam"]&publication_date="5"&price="1000-1500"&availability=["available"]`;
